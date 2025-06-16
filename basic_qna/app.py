dc0a8453bd56720b4aba0fd3d8262bd2de028508
Kushal2205a <introvertedspacemine2@gmail.com>
2025-06-17 05:23:45 +0530

    added file uploader ui and loaded the model

--- basic_qna/app.py
+++ basic_qna/app.py
@@ -1,4 +1,20 @@
 import pdfplumber 
+import streamlit as st
 from sentence_transformers import SentenceTransformer 
 
-model = SentenceTransformer('all-MiniLM-L6-v2')
+
+st.title("Compilance Assistant")
+
+user_uploaded_file = st.file_uploader(label = "Please Upload a report", type = "pdf", accept_multiple_files = False, key = "filee")
+@st.cache_resource 
+def load_model():
+    model = SentenceTransformer(
+        'all-MiniLM-L6-v2',
+        prompts = {
+            "retrieval" : "retreive semantically similar text :"} )
+    return model 
+
+model = load_model()
+
+
+
