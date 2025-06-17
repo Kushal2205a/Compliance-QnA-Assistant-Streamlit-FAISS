a1c30fb93fd4f13cf0e9b044eb578fa981a94fde
Kushal2205a <introvertedspacemine2@gmail.com>
2025-06-17 10:36:26 +0530

    Added Pdf extraction and embedding Functionality

--- basic_qna/app.py
+++ basic_qna/app.py
@@ -1,11 +1,14 @@
 import pdfplumber 
 import streamlit as st
 from sentence_transformers import SentenceTransformer 
+import spacy 
+
 
 
 st.title("Compilance Assistant")
 
 user_uploaded_file = st.file_uploader(label = "Please Upload a report", type = "pdf", accept_multiple_files = False, key = "filee")
+
 @st.cache_resource 
 def load_model():
     model = SentenceTransformer(
@@ -15,6 +18,32 @@ def load_model():
     return model 
 
 model = load_model()
+
+if user_uploaded_file is not None:
+    user_uploaded_file.seek(0)
+
+    with pdfplumber.open(user_uploaded_file) as pdf:
+        text = ""
+        for page in pdf.pages:
+            text += page.extract_text() or ""
+
+    def spacy_chunker(text):
+        nlp = spacy.load("en_core_web_sm")
+        doc = nlp(text)
+        chunked_sentence = [sentences.text for sentences in doc.sents]
+        return chunked_sentence
+
+    actual_chunked_sentence = spacy_chunker(text)
+
+    embeddings = model.encode(actual_chunked_sentence)
+
+
+
+
+
+
+
+
 
 
 
