d00e961e2193d4d3965ee47db2ccbf314db974b7
Kushal2205a <introvertedspacemine2@gmail.com>
2025-06-17 17:50:08 +0530

    Completed the core engine (includes FAISS, Re ranking)

--- basic_qna/app.py
+++ basic_qna/app.py
@@ -2,8 +2,8 @@ import pdfplumber
 import streamlit as st
 from sentence_transformers import SentenceTransformer 
 import spacy 
-
-
+import faiss 
+import numpy as np 
 
 st.title("Compilance Assistant")
 
@@ -27,7 +27,6 @@ if user_uploaded_file is not None:
         for page in pdf.pages:
             text += page.extract_text() or ""
             
-    @st.cache_resource
     def spacy_chunker(text):
         nlp = spacy.load("en_core_web_sm")
         doc = nlp(text)
@@ -37,6 +36,10 @@ if user_uploaded_file is not None:
     actual_chunked_sentence = spacy_chunker(text)
 
     embeddings_pdf = model.encode(actual_chunked_sentence)
+    document_embeddings = np.array(embeddings_pdf).astype("float32")
+    faiss.normalize_L2(document_embeddings)
+    index = faiss.IndexFlatL2(384)
+    index.add(document_embeddings)
 
 
 if "chat_history" not in st.session_state:
@@ -55,11 +58,60 @@ for message in st.session_state["chat_history"]:
     with st.chat_message(message["role"]):
         st.write(message["content"])
 
+top_k = 5 
+
+def rerank(chunk:str, query:str ) ->int :
+    score = 0
+    words = chunk.split()
+
+    if len(words) > 12 :
+        score += 1 
+    
+    if len(words) > 20 :
+        score += 1 
+    
+    if chunk.strip().endswith('.'):
+        score += 1 
+    
+    for word in query.lower().split():
+        if word in chunk.lower():
+            score += 1 
+    
+    if len(words) < 10 :
+        score -= 2
+
+    if len(words) < 5 :
+        score -= 3 
+    
+    return score 
+    
 
 if user_prompt:
     st.chat_message("user").write(user_prompt)
     chat_history.append({"role" : "user", "content" : user_prompt})
-    embeddings_query = model.encode(  "Retreive semantically similar text :" + user_prompt )
+    
+    embeddings_query = model.encode(user_prompt )
+    embeddings_query = np.array(embeddings_query).astype("float32")
+    if embeddings_query.ndim == 1:
+        embeddings_query = embeddings_query.reshape(1,-1)
+
+    D, I = index.search(embeddings_query,top_k)
+
+    matching_chunks = [actual_chunked_sentence[i] for i in I[0]]
+   
+
+    scored_chunks = []
+    for chunk in matching_chunks:
+        score = rerank(chunk, user_prompt)
+        scored_chunks.append((score,chunk))
+    scored_chunks.sort(reverse=True)
+
+    best_match = scored_chunks[0][1]
+    st.chat_message("assistant").write(f"Best match is : \n{best_match}\n\n")
+    for score,chunk in scored_chunks[1:3]:
+        st.chat_message("assistant").write(f"🔷{chunk}")
+    chat_history.append({"role" : "assistant", "content" : best_match})
+    
 
 
 
