6aaac33e7b8795b3228bf743b9bf7216a68c4902
Kushal2205a <introvertedspacemine2@gmail.com>
2025-06-17 13:24:58 +0530

    Added Chat Functionality

--- basic_qna/app.py
+++ basic_qna/app.py
@@ -26,7 +26,8 @@ if user_uploaded_file is not None:
         text = ""
         for page in pdf.pages:
             text += page.extract_text() or ""
-
+            
+    @st.cache_resource
     def spacy_chunker(text):
         nlp = spacy.load("en_core_web_sm")
         doc = nlp(text)
@@ -35,7 +36,35 @@ if user_uploaded_file is not None:
 
     actual_chunked_sentence = spacy_chunker(text)
 
-    embeddings = model.encode(actual_chunked_sentence)
+    embeddings_pdf = model.encode(actual_chunked_sentence)
+
+
+if "chat_history" not in st.session_state:
+    st.session_state["chat_history"] = []
+
+chat_history = st.session_state["chat_history"]
+
+if st.button("Clear Chat"):
+    st.session_state["chat_history"].clear()
+user_prompt = st.chat_input("Say Something")
+
+#messages = st.container(height=800)
+
+
+for message in st.session_state["chat_history"]:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+
+if user_prompt:
+    st.chat_message("user").write(user_prompt)
+    chat_history.append({"role" : "user", "content" : user_prompt})
+    embeddings_query = model.encode(  "Retreive semantically similar text :" + user_prompt )
+
+
+
+
+
 
 
 
